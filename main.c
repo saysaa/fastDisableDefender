a519c803b7391e873e6d4782e992ff3dd8a12f3d
Saysaa <112180175+saysaa@users.noreply.github.com>
2025-08-12 17:55:50 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <windows.h>
+
+int IsRunAsAdmin() {
+    BOOL isAdmin = FALSE;
+    PSID adminGroup = NULL;
+    SID_IDENTIFIER_AUTHORITY NtAuthority = SECURITY_NT_AUTHORITY;
+
+    if (AllocateAndInitializeSid(&NtAuthority, 2,
+        SECURITY_BUILTIN_DOMAIN_RID,
+        DOMAIN_ALIAS_RID_ADMINS,
+        0, 0, 0, 0, 0, 0,
+        &adminGroup))
+    {
+        CheckTokenMembership(NULL, adminGroup, &isAdmin);
+        FreeSid(adminGroup);
+    }
+    return isAdmin;
+}
+
+int main(void) {
+    if (!IsRunAsAdmin()) {
+        SHELLEXECUTEINFO sei = { sizeof(sei) };
+        sei.lpVerb = "runas";
+        sei.lpFile = "fastDisableDefender.exe"; // nom du .exe
+        sei.nShow = SW_SHOWNORMAL;
+
+        if (!ShellExecuteEx(&sei)) {
+            MessageBox(NULL, "ELEVATION PRIVILEGE ERROR: Please execute fastDisableDefender in admin.", "Erreur", MB_ICONERROR);
+            return 1;
+        }
+        return 0;
+    }
+
+    printf(">> fastDisableDefender\n");
+    char menu_choose[30];
+
+    printf("'activate' <> 'disable'\n");
+    while (1) {
+        printf(">> ");
+        scanf("%29s", menu_choose);
+
+        if (strcmp(menu_choose, "activate") == 0) {
+            printf("Activating Defender...\n");
+            Sleep(500);
+            system("powershell -Command \"Set-MpPreference -EnableRealtimeMonitoring $true -Force\"");
+            printf("SUCCESS: Windows Defender enabled!\n");
+            break;
+        }
+        else if (strcmp(menu_choose, "disable") == 0) {
+            printf("Disabling Defender...\n");
+            Sleep(500);
+            system("powershell -Command \"Set-MpPreference -DisableRealtimeMonitoring $true -Force\"");
+            printf("SUCCESS: Windows Defender disabled!\n");
+            Sleep(500);
+            break;
+        }
+        else {
+            printf("Invalid syntax :/\n");
+        }
+    }
+    return 0;
+}
